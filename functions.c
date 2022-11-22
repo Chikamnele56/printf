ddd3b8749431fd4890fab0ceb76b4d30566f6e05
Osas <oseikponmwosaosayande@gmail.com>
2022-11-22 15:18:19 -0800

    printf

--- functions.c
+++ functions.c
@@ -90,7 +90,7 @@ int print_percent(va_list types, char buffer[],
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	return (write(1, "&&", 1));
+	return (write(1, "%%", 1));
 }
 /************************* PRINT INT *************************/
 /**
@@ -153,7 +153,7 @@ int print_binary(va_list types, char buffer[],
 	UNUSED(prcision);
 	UNUSED(size);
 	n = va_arg(types, unsigned int);
-	m = 2147483648; /* (2 ^ 31) */
+	m = 2147483648; 
 	a[0] = n / m;
 	for (i = 1; i < 32; i++)
 	{
